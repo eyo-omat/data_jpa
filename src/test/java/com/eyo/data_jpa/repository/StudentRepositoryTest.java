ce963e42738338b873b2aeefc12c9fd194e6e064
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-01-17 19:01:56 +0000

    :clown_face: Added Test class StudentRepository interface with saveStudent method

---
+++ src/test/java/com/eyo/data_jpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.eyo.data_jpa.repository;
+
+import com.eyo.data_jpa.model.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent() {
+        Student student = Student.builder()
+                .firstName("Kojo")
+                .lastName("duncan")
+                .email("kjk@mail.com")
+                .age(76)
+                .guardianName("Thanos")
+                .guardianEmail("snap@mail.com")
+                .guardianMobile("0.5")
+                .build();
+
+        studentRepository.save(student);
+    }
+}
